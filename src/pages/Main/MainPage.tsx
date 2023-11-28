96d518e83895c50758db49b903d4d78d5e3fa36c
hwangjaehyeong <ghkdwogud852@gmail.com>
2023-11-28 18:48:35 +0900

    feat : ThreeTest christmas tree 추가

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -10,7 +10,8 @@ export const MainPage: FC<MainPageProps> = ({ className }) => {
   return (
     <Root className={className}>
       <Container>
-        <ThreeTest />
+        <ThreeTest position={[300, 150, -100]} url={'christmas_tree_01.fbx'} />
+        <ThreeTest position={[700, 300, -150]} fov={130} url={'christmas_tree_02.fbx'} />
       </Container>
     </Root>
   )
--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export const Root = styled.div`
   width: 100%;
+  min-height: 100vh;
+  background: #eeee;
   display: flex;
   justify-content: center;
 `
@@ -10,5 +12,5 @@ export const Container = styled.div`
   width: 1280px;
   display: flex;
   justify-content: center;
-  padding-top: 50px;
+  padding-top: 150px;
 `
