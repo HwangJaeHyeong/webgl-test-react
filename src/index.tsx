ac99d075308fa30cbb09c9a3219f1951a9f275a6
hwangjaehyeong <ghkdwogud852@gmail.com>
2023-11-28 16:20:18 +0900

    refactor: OpenCvProvider 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 import 'antd/dist/antd.css'
-import { OpenCvProvider } from 'opencv-react'
 import { MainPage } from 'pages/Main'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -9,11 +8,9 @@ import './default.css'
 const root = ReactDOM.createRoot(document.getElementById('root') as any)
 
 root.render(
-  <OpenCvProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-      </Routes>
-    </BrowserRouter>
-  </OpenCvProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  </BrowserRouter>
 )
